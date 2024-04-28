8d42b65986a995eedb4a9f198035b39eed9401a5
Stephanie Xue <stephaniexue3@gmail.com>
2024-04-27 17:20:40 -0700

    add function to convert md to html

--- CS50-3-Django/wiki/encyclopedia/views.py
+++ CS50-3-Django/wiki/encyclopedia/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
 
+# Converts markdown content to html
+def md_to_html(content):
+    markdowner = Markdown()
+    return markdowner.convert(content)
+    
+
+# Homepage with list of encyclopedia entries (in alphabetical order)
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
