db5957f5d36f76fa9d7b3a4ba5f8a412725b83de
Stephanie Xue <stephaniexue3@gmail.com>
2024-04-27 20:57:57 -0700

    fix save_edit bug

--- CS50-3-Django/wiki/encyclopedia/urls.py
+++ CS50-3-Django/wiki/encyclopedia/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("search", views.search, name="search"),
     path("create", views.create, name="create"),
     path("edit", views.edit, name="edit"),
+    path("save_edit", views.save_edit, name="save_edit"),
     path("delete", views.delete, name="delete"),
     path("random", views.randomchoice, name="random")
 ]
