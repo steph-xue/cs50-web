0d37e154c83b7240ddcd60c76bf39df57d67e16d
Stephanie Xue <stephaniexue3@gmail.com>
2024-05-02 17:33:19 -0700

    add individual listing pages for the auctions

--- CS50-4-SQLModelsMigrations/commerce/auctions/models.py
+++ CS50-4-SQLModelsMigrations/commerce/auctions/models.py
@@ -2,5 +2,34 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+# Model for user database (username, email, password)
 class User(AbstractUser):
+    pass
+
+
+# Model for category of auction items database
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
+
+
+# Model for auction listings database (title, description, image url, current price, category, owner of the posting)
+class Listing(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    image_url = models.CharField(max_length=300)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="listing_category")
+    is_active = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="listing_user")
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    pass
+
+class Bid(models.Model):
     pass
--- CS50-4-SQLModelsMigrations/commerce/auctions/views.py
+++ CS50-4-SQLModelsMigrations/commerce/auctions/views.py
@@ -3,14 +3,21 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from .models import Category, Listing, Comment, Bid
 
 from .models import User
 
 
+# Allows the user to view the homepage with all active listings
 def index(request):
-    return render(request, "auctions/index.html")
+    active_listings = Listing.objects.filter(is_active=True)
+    return render(request, "auctions/index.html",
+    {
+        "listings": active_listings
+    })
 
 
+# Allows the user to log in
 def login_view(request):
     if request.method == "POST":
 
@@ -31,11 +38,13 @@ def login_view(request):
         return render(request, "auctions/login.html")
 
 
+# Allows the user to log out
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 
+# Allows the user to register as a new user
 def register(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -61,3 +70,16 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+    
+
+# Allows the user to view a specific auction listing page
+def listing(request, id):
+    listing_data = Listing.objects.get(pk=id)
+    return render(request, "auctions/listing.html",
+    {
+        "listing": listing_data
+    })
+
+# Allows the user to create a new listing
+def create(request):
+    pass
