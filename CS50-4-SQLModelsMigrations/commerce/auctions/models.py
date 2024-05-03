ddde1ba2a18eff96d9176971085ac91fd8e379b8
Stephanie Xue <stephaniexue3@gmail.com>
2024-05-03 12:05:54 -0700

    add bid to listing model

--- CS50-4-SQLModelsMigrations/commerce/auctions/models.py
+++ CS50-4-SQLModelsMigrations/commerce/auctions/models.py
@@ -13,6 +13,16 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+    
+
+# Model for bid database (bid price, listing item, user who made the bid)
+class Bid(models.Model):
+    bid = models.FloatField()
+    listing_item = models.ForeignKey(Listing, on_delete=models.CASCADE, blank=True, null=True, related_name="bid_listing")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="bid_user")
+
+    def __str__(self):
+        return f"{self.bid} bid by {self.user} on {self.listing_item}"
 
 
 # Model for auction listings database (title, description, image url, current price, category, in which users watchlists, owner of the posting)
@@ -20,7 +30,8 @@ class Listing(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
     image_url = models.CharField(max_length=300)
-    initial_price = models.FloatField()
+    initial_price = models.FloatField(),
+    current_bid = models.ForeignKey(Bid, on_delete=models.CASCADE, blank=True, null=True, related_name="listing_bid")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="listing_category")
     is_active = models.BooleanField(default=True)
     watchlist = models.ManyToManyField(User, blank=True, null=True, related_name="user_watchlist")
@@ -28,7 +39,9 @@ class Listing(models.Model):
 
     def __str__(self):
         return self.title
+    
 
+# Model for comment database (comment, date and time, listing item, and user who made the comment)
 class Comment(models.Model):
     text = models.CharField(max_length=300)
     date_time = models.DateTimeField()
@@ -39,11 +52,4 @@ class Comment(models.Model):
         return f"{self.text} commented by {self.user} on {self.listing_item}"
 
 
-class Bid(models.Model):
-    bid = models.FloatField()
-    listing_item = models.ForeignKey(Listing, on_delete=models.CASCADE, blank=True, null=True, related_name="bid_listing")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="bid_user")
-
-    def __str__(self):
-        return f"{self.bid} bid by {self.user} on {self.listing_item}"
 
