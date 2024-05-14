d1285f47754289563f6d79f538f4948757841841
Stephanie Xue <stephaniexue3@gmail.com>
2024-05-13 20:26:13 -0700

    add comments to inbox.js file

--- CS50-5,6-JavaScript,UserInterface/mail/mail/static/mail/inbox.js
+++ CS50-5,6-JavaScript,UserInterface/mail/mail/static/mail/inbox.js
@@ -1,7 +1,7 @@
-//
+// When the web application loads, directs the user to the inbox by default and detects events (button clicks and form submissions)
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Use buttons to toggle between views
+  // Use buttons to toggle between views on the navigation bar
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
@@ -23,14 +23,14 @@ function compose_email() {
   document.querySelector('#email-details-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
-  // Clear out composition fields
+  // Clear out composition fields (empty out the form)
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
 }
 
 
-//
+// Allows the user to send the composed email
 function send_email(event) {
 
   event.preventDefault();
@@ -40,7 +40,7 @@ function send_email(event) {
   const subject = document.querySelector('#compose-subject').value;
   const body = document.querySelector('#compose-body').value;
 
-  // Send information to the backend to create and save new email objects (sent by the user)
+  // Send information to the backend to create and save new email objects for each user/recipient (sent by the user)
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
@@ -57,13 +57,11 @@ function send_email(event) {
 
     // Redirects user to the sent inbox
     load_mailbox('sent');
-
   });
-
 }
 
 
-//
+// Allows the user to view an email and its details, as well as reply, read/unread, and archive/unarchive the email
 function view_mail(id) {
 
   // Show email details view and hide other views
@@ -114,6 +112,7 @@ function view_mail(id) {
     replybutton.innerHTML = 'Reply';
     replybutton.classList.add('p-2', 'mt-2', 'mr-2', 'btn', 'btn-primary');
     replybutton.addEventListener('click', function() {
+      // Compose email is prefilled with the sender, subject (prepended with an 'Re:'), and details of the previous email sent 
       compose_email();
       document.querySelector('#compose-recipients').value = email.sender;
       let subject = email.subject;
@@ -121,13 +120,13 @@ function view_mail(id) {
         subject = 'Re: ' + subject;
       }
       document.querySelector('#compose-subject').value = subject;
-      document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`
+      document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`;
     });
     document.querySelector('#email-details-view').append(replybutton);
 
     // Create button to read/unread email (redirects user to the inbox mailbox)
     const readbutton = document.createElement('button');
-    readbutton.innerHTML = email.read? 'Unread' : 'Read';
+    readbutton.innerHTML = email.read ? 'Unread' : 'Read';
     readbutton.classList.add('p-2', 'mt-2', 'mr-2', 'btn', 'btn-secondary');
     readbutton.addEventListener('click', function() {
       fetch(`/emails/${id}`, {
@@ -163,7 +162,8 @@ function view_mail(id) {
 }
 
 
-//
+// Allows the user to load the selected mailbox (inbox, sent, or archive)
+// All emails in the selected mailbox are displayed in reverse chronological order
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
@@ -175,7 +175,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').innerHTML = `<h3 id="title">${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   document.querySelector('#title').classList.add('pb-3');  
 
-  // Use the API route to get all the mail from the mailbox chosen (inbox, sent, or archive)
+  // Use the API route to get all the emails from the mailbox chosen (inbox, sent, or archive)
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
@@ -186,7 +186,7 @@ function load_mailbox(mailbox) {
       // Iterate through each of the emails
       emails.forEach(email => {
 
-        // Create div element for each email to display in the mailbox
+        // Create div element for each email to display in the mailbox (shows the sender, subject, timestamp)
         const emaildiv = document.createElement('div');
         emaildiv.classList.add('row', 'greyborder');
         emaildiv.innerHTML = `
@@ -208,9 +208,9 @@ function load_mailbox(mailbox) {
           emaildiv.classList.add('unread');
         }
 
-        // Add event handler for when any mail is clicked on
+        // Add event handler for when any mail is clicked on to view email details
         emaildiv.addEventListener('click', function() {
-            view_mail(email.id);
+            view_mail(email.id)
         });
 
         // Add each div element to the mailbox view section
