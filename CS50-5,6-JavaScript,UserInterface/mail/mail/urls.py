f6f90842bc464122d4f9b608f2ddc6fbe487a605
Stephanie Xue <stephaniexue3@gmail.com>
2024-05-13 20:41:05 -0700

    add comments to views.py

--- CS50-5,6-JavaScript,UserInterface/mail/mail/urls.py
+++ CS50-5,6-JavaScript,UserInterface/mail/mail/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Web application Routes
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
