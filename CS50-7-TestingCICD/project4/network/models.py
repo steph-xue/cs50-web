e3960e188bdd1b5939f56a7ba4b8dc5a8dfcd55d
Stephanie Xue <stephaniexue3@gmail.com>
2024-05-29 15:24:29 -0700

    added Post model

---
+++ CS50-7-TestingCICD/project4/network/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    content = models.CharField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_user")
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.id} was posted by {self.user.username} on {self.date_time.strftime('%m/%d/%Y, %H:%M:%S')}"
